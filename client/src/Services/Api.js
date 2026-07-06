fe002a97af863b272820b37fab7af5439252afc5
Shivam Kumar Singh <000shiv2304@gmail.com>
2026-07-07 01:44:39 +0530

    fix backend env name

--- client/src/Services/Api.js
+++ client/src/Services/Api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:    import.meta.env.VITE_BACKEND_URL || "http://localhost:5000", 
+  baseURL:    import.meta.env.BACKEND_URL || "http://localhost:5000", 
   withCredentials: true
 });
 
 export default api;
+        
