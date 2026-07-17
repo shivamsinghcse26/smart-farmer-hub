73c7bf51452faf71cd256936872694a90f4b8543
Shivam Kumar Singh <000shiv2304@gmail.com>
2026-07-17 23:14:24 +0530

    import env fix

--- client/src/Services/Api.js
+++ client/src/Services/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+ 
 const api = axios.create({
-  baseURL:   BACKEND_URL || "http://localhost:5000", 
+  baseURL:   import.meta.env.VITE_BACKEND_URL|| "http://localhost:5000", 
   withCredentials: true
 });
 
