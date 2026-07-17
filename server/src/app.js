27a5ce924d1fc1592974d146946c98f871c96966
Shivam Kumar Singh <000shiv2304@gmail.com>
2026-07-17 23:24:05 +0530

    fix cors origin

--- server/src/app.js
+++ server/src/app.js
@@ -21,7 +21,9 @@ app.use(
       process.env.FRONTEND_URL,
       "http://localhost:5173"
     ],
-    credentials: true
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
